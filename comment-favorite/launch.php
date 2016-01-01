1517d731b2b43c67f294a260485a7e1abb3ea139
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-01-01 21:07:18 +0700

    Updates

--- comment-favorite/launch.php
+++ comment-favorite/launch.php
@@ -1,10 +1,11 @@
 <?php
 
-$cfv_config = File::open(PLUGIN . DS . File::B(__DIR__) . DS . 'states' . DS . 'config.txt')->unserialize();
+$cfv_config = File::open(__DIR__ . DS . 'states' . DS . 'config.txt')->unserialize();
 
-Filter::add('shield:lot', function($data) use($cfv_config) {
-    if(isset($data['article']->comments) && $data['article']->comments !== false) {
-        foreach($data['article']->comments as &$comment) {
+Filter::add('shield:lot', function($data) use($config, $cfv_config) {
+    $c = $config->page_type;
+    if(isset($data[$c]->comments) && $data[$c]->comments !== false) {
+        foreach($data[$c]->comments as &$comment) {
             if(isset($comment->fields->comment_favorite)) {
                 $comment->name = sprintf($cfv_config['marker'], $comment->name);
                 $comment->message = sprintf($cfv_config['container'], $comment->message);
@@ -17,6 +18,6 @@ Filter::add('shield:lot', function($data) use($cfv_config) {
 
 if(trim($cfv_config['css']) !== "") {
     Weapon::add('shell_after', function() use($config, $cfv_config) {
-        echo O_BEGIN . '<style media="screen">' . ($config->html_minifier ? Converter::detractShell($cfv_config['css']) : $cfv_config['css']) . '</style>' . O_END;
+        echo O_BEGIN . '<style media="screen">' . $cfv_config['css'] . '</style>' . O_END;
     });
 }
